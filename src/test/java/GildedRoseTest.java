e12f13b5e7902ceb9bbbc1fc4c633ace6b3ec67d
anshutyagi9 <anshutyagi9@gmail.com>
2022-01-06 23:02:06 +0530

    refactor

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -49,74 +49,59 @@ public class GildedRoseTest {
     @Test
     public void item_AgedBrie_increasesInQuality() {
         GildedRose app = new GildedRose( new Item[]{new Item("Aged Brie", 2, 2)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Aged Brie", 1, 3));
     }
 
     @Test
     public void item_AgedBrie_increasesInQuality_DoublesWhenOff() {
         GildedRose app = new GildedRose( new Item[]{new Item("Aged Brie", 0, 2)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Aged Brie", -1, 4));
     }
 
     @Test
     public void item_AgedBrie_cannotGoOver50Quality() {
         GildedRose app = new GildedRose( new Item[]{new Item("Aged Brie", 2, 50)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Aged Brie", 1, 50));
     }
 
     @Test
     public void item_Sulfuras_neverChanges() {
         GildedRose app = new GildedRose( new Item[]{new Item("Sulfuras, Hand of Ragnaros", 100, 100)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Sulfuras, Hand of Ragnaros", 100, 100));
     }
 
     @Test
     public void item_BackStagePasses_increasesInQuality_byOneOutside10Days() {
         GildedRose app = new GildedRose( new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 20, 2)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Backstage passes to a TAFKAL80ETC concert", 19, 3));
     }
 
     @Test
     public void item_BackStagePasses_increasesInQuality_byTwoInside10Days() {
         GildedRose app = new GildedRose( new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 2)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Backstage passes to a TAFKAL80ETC concert", 9, 4));
     }
 
     @Test
     public void item_BackStagePasses_increasesInQuality_byThreeInside5Days() {
         GildedRose app = new GildedRose( new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 2)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Backstage passes to a TAFKAL80ETC concert", 4, 5));
     }
 
     @Test
-    public void item_BackStagePasses_increasesInQuality_goesToZeroWhenSellInExpires() {
+    public void itemBackStagePasses_increasesInQuality_goesToZeroWhenSellInExpires() {
         GildedRose app = new GildedRose( new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)});
-
         app.updateQuality();
-
         assertItemEquals(app.items[0], new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0));
     }
+
     public static void assertItemEquals(Item actual, Item expected) {
         assertEquals(expected.name, actual.name);
         assertEquals(expected.quality, actual.quality);
